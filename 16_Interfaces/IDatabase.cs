24d36d455c4f00f3061be35b2dc748ec9a647009
Nesibe <nesibe.yaylaci@gmail.com>
2023-03-06 13:56:04 +0300

    some changes

---
+++ 16_Interfaces/IDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_Interfaces
+{
+    internal interface IDatabase
+    {
+        // Bu Interface'de sadece metod yapılarını kodsuz olarak tanımlıyoruz. Yani şablonu oluşturuyoruz
+
+        int login(string userName, string password); // Database imizde giriş yapan metod
+
+        void close(); // Database mizi kapatıyoruz.
+
+        void addValue(string value); // DataBase imize bir değer ekliyor
+
+        void deleteValue(string value); // DataBase imizden değer siler
+
+        void updateValue(string value); // DataBase imizde güncelleme yapar
+
+        void getValue(); // DataBase imizden değer getirir
+    }
+}
