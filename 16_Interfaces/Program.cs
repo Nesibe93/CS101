f46eea6f634f2c4ca93c08324250678538e25b1a
Nesibe <nesibe.yaylaci@gmail.com>
2023-03-06 15:13:04 +0300

    some changes

--- 16_Interfaces/Program.cs
+++ 16_Interfaces/Program.cs
@@ -11,10 +11,20 @@ internal class Program
 
         clsDbManager database;
 
+        // sorgulama işlemleri //
+
+        Console.WriteLine("< Veritabanı bağlantıları> \n");
+        Console.WriteLine("[1] - Oracle Database Bağlantısı kur");
+        Console.WriteLine("[2] - MySql Database Bağlantısı kur");
+        Console.WriteLine("[3] - Oracle Database Bağlantısı kur");
+        Console.Write("-> SEçiminiz:");
+
+        option = Convert.ToInt32(Console.ReadLine());
 
         switch (option)
         {
             case 1:
+                database =
 
                 break;
 
